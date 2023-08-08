7b2c069cbc6683007cfb09db80e553b7953015a6
notmuchwby <yamakasy1337@gmail.com>
2023-08-08 19:39:41 +0600

    first import frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import AuthorizatitonPage from './components/auth/AuthorizationPage';
 import WelcomePage from './components/welcome/WelcomePage';
+import HomePage from './components/home-page/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <AuthorizatitonPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path='/signin' element={<AuthorizatitonPage/>}/>
+          <Route path='/welcome' element={<WelcomePage/>}/>
+          <Route path='/home' element={<HomePage/>}/>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
--- src/components/auth/AuthorizationPage.jsx
+++ src/components/auth/AuthorizationPage.jsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
 import './style.css';
 import { signIn } from "../../services/auth";
+import { useNavigate } from "react-router-dom";
 
 const AuthorizatitonPage = () => {
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         signIn(login, password)
             .then((response) => {
-                console.log(response)
+                localStorage.setItem('token', response.token);
+                console.log(response.token);
+                navigate('/home');
             })
-            .catch(e => console.log(e))
+            .catch(e => {
+                setErrorMessage('There was an error while trying to sign in!')
+                console.log(e)
+            })
 
 
         setLogin('');
@@ -21,25 +29,48 @@ const AuthorizatitonPage = () => {
     }
     
     return (
-        <> 
-            Log into your account 
-
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Enter your login: </label>
-                    <input value={login} type="text" onChange={e => setLogin(e.target.value)}/>
+        <div className='auth-page'> 
+            <div className='left-side-container'>
+                <div className='left-side'>
+                     <h2>
+                         Log into your account
+                     </h2>
+            
+                     <form onSubmit={handleSubmit}>
+                         <div className='login-container'>
+                             <label>Enter your login: </label>
+                             <input value={login} type="text" onChange={e => setLogin(e.target.value)}/>
+                         </div>
+            
+                         <div className='password-container'>
+                             <label>Enter your password: </label>
+                             <input value={password} type="text" onChange={e => setPassword(e.target.value)}></input>
+                         </div>
+            
+                         <div>
+                             <button type="submit">Submit</button>
+                         </div>
+                     </form>
+            
+                     {errorMessage && 
+                     <div>
+                         {errorMessage}
+                     </div>}
                 </div>
+            </div>
 
-                <div>
-                    <label>Enter your password: </label>
-                    <input value={password} type="text" onChange={e => setPassword(e.target.value)}></input>
-                </div>
 
-                <div>
-                    <button type="submit">Submit</button>
+           <div className='right-side-container'>
+                <div className='right-side'>
+                    <div>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Odio incidunt inventore cumque, sequi velit ipsa natus minima consequuntur magni,
+                        nihil hic beatae ad reiciendis facere quaerat voluptatibus.
+                        Omnis, inventore eos.
+                    </div>
                 </div>
-            </form>
-        </>
+           </div>
+        </div>
     )
 
 }   
